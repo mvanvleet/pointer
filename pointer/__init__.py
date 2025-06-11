8f0913e0fe401d752247887be084db5c8fedfec6
mvanvleet <mvanvleet@spelman.edu>
2025-06-11 14:55:35 -0400

    Make path dependency to data directory system agnostic.

--- pointer/__init__.py
+++ pointer/__init__.py
@@ -1,6 +1,9 @@
 """Parameter Optimization for INTERmolecular force fields fit to quantum-mechanical data."""
 
-# Add imports here
 from . import quantum
 
 from ._version import __version__
+
+# Make sure modules have access to the data files directory
+from pathlib import PurePath
+DATADIR = PurePath(__file__).parent / 'data'
--- pointer/tests/test_ionization.py
+++ pointer/tests/test_ionization.py
@@ -3,16 +3,19 @@ Unit and regression test for the ionization energy module.
 """
 
 import sys
+from pathlib import PurePath
 
 import pytest
 
 from pointer.quantum import molpro
+from .. import DATADIR
 
 
 def test_molpro_process_ip():
     """Test parsing of Molpro IP Calculation Files"""
 
-    ip, homo = molpro.process_ionization("data/quantum/h2o_ip.out")
+    ofile = PurePath(DATADIR,'quantum/','h2o_ip.out')
+    ip, homo = molpro.process_ionization(ofile)
     expected_ip, expected_homo = 0.46602931, -0.333967
 
     assert pytest.approx(ip) == expected_ip
